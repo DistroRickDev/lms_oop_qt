0939f939cfdbd475f9d8a382e0a6ed27a4e0feb7
rdntintern <rdntintern@gmail.com>
2020-12-26 15:19:04 +0000

    added few features

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,19 +20,29 @@ void MainWindow::submit_btn_clicked()
     library_books new_book(ui->author_input->text(), ui->isbn_input->text().toULong(), ui->genre_input->text(), ui->title_input->text(), random());
     bMap.insert(bc, new_book);
     bc ++;
-    print_map();
+    //print_map();
+    write_to_file();
 }
 
 void MainWindow::write_to_file()
 {
     QFile book_io("books.txt");
-    if(!book_io.open(QIODevice::Append | QIODevice::Text))
+    if(!book_io.open(QIODevice::ReadWrite | QIODevice::Text))
     {
         qDebug()<< "Error Loading Data File\n";
     }
     QTextStream out(&book_io);
-    out << "The magic number is: " << 49 << "\n";
+    //out << "The magic number is: " << 49 << "\n";
+    for (auto it : bMap)
+    {
+        out << it.get_title() << Qt::endl;
+        out << it.get_author()<< Qt::endl;
+        out << it.get_genre()<< Qt::endl;
+        out << it.get_isbn()<< Qt::endl;
+        out << it.get_id()<< Qt::endl;
+    }
     book_io.close();
+    qDebug() << "FILE WRITTEN" << Qt::endl;
 }
 
 
